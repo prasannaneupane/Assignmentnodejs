704b6e0a3531b70db498d6c13aa54c312d11c632
Prasanna <128725370+prasannaneupane@users.noreply.github.com>
2025-01-19 20:17:17 +0545

    complete upto 10

--- 1no.js
+++ 1no.js
@@ -1,5 +1,7 @@
 //1. Write a program that prints all even numbers from 1 to 50 using proper indentation and comments to explain your code.
 
-for(let i=1;i<50;i++){
-    console.log(i+2)
+for(let i=0;i<=50;i++){
+    if (i%2==0){
+        console.log(i);
+    }
 }
---
+++ 5no.js
@@ -0,0 +1,22 @@
+//Declare three variables: one using var, one using let, and one using const. Try reassigning values to them and note any errors or behaviors.
+
+
+var a=55
+console.log(a) //output: 55
+let b="hello"
+console.log(b) //output: hello
+const c = 28
+console.log(c)
+
+var a=57
+console.log(a) // output: 57
+
+// //throws error 
+// let b="Hey"
+// console.log(b) //cannot redeclare block-scoped variable 'b'
+
+// const c=60
+// console.log(c) //cannot redeclare block-scoped variable 'c'
+
+
+
